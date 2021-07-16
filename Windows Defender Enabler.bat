3051c8bd72a7e7daa6cd8aa53262ac4ee99debf2
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-07-16 11:58:31 +0530

    Delete Windows Defender Enabler.bat

--- Windows Defender Enabler.bat
+++
@@ -1,9 +0,0 @@
-Powershell Set-Service -Name WdNisSvc -StartupType Automatic
-Powershell Set-Service -Name WinDefend -StartupType Automatic
-cd "C:\ProgramData\Microsoft\Windows Defender"
-ren Platform1 Platform
-cd "C:\Program Files"
-ren "Windows Defender1" "Windows Defender"
-Powershell Start-Service -Name WinDefend 
-Powershell Start-Service -Name WdNisSvc 
-exit
